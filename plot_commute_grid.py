7f359717850ace12f92b8ed2e1c85b535542b5ed
Shea Garrison-Kimmel <sheagk@gmail.com>
2019-07-01 10:58:35 -0700

    it works!

--- plot_commute_grid.py
+++ plot_commute_grid.py
@@ -8,17 +8,23 @@ from itertools import product
 from collections import OrderedDict
 
 import bokeh.plotting as bk
+from bokeh.io import export_png
 from bokeh.models import map_plots, Range1d, GMapOptions
 from bokeh.models.glyphs import Patches, Line, Circle
 from bokeh.models import HoverTool, PanTool, WheelZoomTool, BoxSelectTool, ResetTool, ColorBar
 from bokeh.models.mappers import ColorMapper, LinearColorMapper
-from bokeh.palettes import Viridis5
+from bokeh.palettes import Viridis256
 from bokeh.layouts import gridplot
+
+def restructure_key(key):
+    name,dest = key.split('_')
+    dest = 'to '+dest[2:]
+    return name + ' ' + dest
 
 basedir = os.path.realpath(__file__).rsplit('/', 1)[0]
 api_file = basedir+'/api_key'
 with open(api_file, 'r') as f:
-    key = f.readline()
+    api_key = f.readline()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pickle_dump')
@@ -29,57 +35,62 @@ with open(args.pickle_dump, 'rb') as f:
     print(f"Loading from {args.pickle_dump}")
     data = pickle.load(f)
 
-lats = data.pop('lat')
-longs = data.pop('long')
+center_lats = np.array(data.pop('lat'))
+center_longs = np.array(data.pop('long'))
 names = set(k.split('_')[0] for k in data)
 
-plots = []
-bk.output_file(args.output_file, mode="cdn") # title="Berlin population")
-moptions = GMapOptions(lat=34.053695, lng=-118.430208, zoom=13, map_type="roadmap")
-# moptions = map_plots.GMapOptions(map_type=)
+dx = np.max(center_longs[1:] - center_longs[:-1])/2
+dy = np.max(center_lats[1:] - center_lats[:-1])/2
 
-def restructure_key(key):
-    name,dest = key.split('_')
-    dest = 'to '+dest[2:]
-    return name + ' ' + dest
+xcoords = [[xc-dx, xc-dx, xc+dx, xc+dx] for xc in center_longs]
+ycoords = [[yc-dy, yc+dy, yc+dy, yc-dy] for yc in center_lats]
+print(f"Plotting {len(xcoords)} squares")
+
+plots = []
+bk.output_file(args.output_file, title="Commute times") , #mode="inlne")
+moptions = GMapOptions(lat=34.053695, lng=-118.430208, zoom=13, map_type="roadmap")
 
 allkeys = [f'{name}_{destkey}' for name, destkey in product(names, ['towork', 'tohome'])]
 dsets = {restructure_key(key): data[key] for key in allkeys}
 
-color_mapper = LinearColorMapper(palette=Viridis5, low=15, high=75)
+color_mapper = LinearColorMapper(palette=Viridis256, low=15, high=75)
 for name in names:
     for destkey in ['towork', 'tohome']:
         key = f'{name}_{destkey}'
         colors = data[key]
 
-        plot = bk.gmap(google_api_key=key, map_options=moptions, title=restructure_key(key))
-
-        source_patches = bk.ColumnDataSource(
-            data=dict(xs=lats, ys=longs,
-                      colors=colors, **dsets))
-
-        patches = Patches(xs="xs", ys="ys", fill_alpha=0.5,
-                         fill_color={"field": "colors", "transform": color_mapper},
-                         line_color='black', line_width=0.25)
-
-        patches_glyph = plot.add_glyph(source_patches, patches)
-
+        plot = bk.gmap(api_key, map_options=moptions, title=restructure_key(key))
         plot.add_tools(PanTool(), WheelZoomTool(), BoxSelectTool(), HoverTool(), 
                 ResetTool())
 
+        # source_patches = bk.ColumnDataSource(
+        #     data=dict(xs=longs, ys=lats,
+        #               colors=colors, **dsets))
+
+        # patches_glyph = plot.square('xs', 'ys', 
+        #     fill_color={"field": "colors", "transform": color_mapper},
+        #     line_color='black', line_width=0.25, source=source_patches)
+
+        source_patches = bk.ColumnDataSource(
+            data=dict(xs=xcoords, ys=ycoords,
+                      colors=colors))
+        patches_glyph = plot.patches('xs', 'ys', fill_alpha=0.5, 
+            fill_color={"field": "colors", "transform": color_mapper},
+            line_color='black', line_width=0.25, source=source_patches)
+
+        # # patches = Patches(xs="xs", ys="ys", fill_alpha=0.5,
+        # #                  fill_color={"field": "colors", "transform": color_mapper},
+        # #                  line_color='black', line_width=0.25)
+        # # patches_glyph = plot.add_glyph(source_patches, patches)
+
+
         hover = plot.select(dict(type=HoverTool))
-        #hover.snap_to_data = False # not supported in new bokeh versions
-        hover.tooltips = OrderedDict([
-            (k, "@"+k) for k in dsets])
-        #     ("Borough", "@boroughsnames"),
-        #     ("Density (pop/km2)", "@density"),
-        # #    ("Population", "@population"),
-        #     ("Area (km2)", "@area"),
-        # #    ("(long,lat)", "($x, $y)"),
-        # ])
+        hover.tooltips = None
+        # hover.tooltips = OrderedDict([
+        #     (k, "@"+k) for k in dsets])
+
         color_bar = ColorBar(color_mapper=color_mapper, border_line_color=None, location=(0,0))
         plot.add_layout(color_bar, 'right')
-
         plots.append(plot)
 
 grid = gridplot(plots, ncols=2)
