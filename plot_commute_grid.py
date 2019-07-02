3d6cee34142fbd1b1cd65b01d42ff631d86edb4a
Shea Garrison-Kimmel <sheagk@gmail.com>
2019-07-01 23:14:21 -0700

    adds plot showing region where no and 1 commute are 'bad'

--- plot_commute_grid.py
+++ plot_commute_grid.py
@@ -21,25 +21,37 @@ def restructure_key(key):
     dest = 'to '+dest[2:]
     return name + ' ' + dest
 
-def plot_patches_on_gmap(vertex_xcoords, vertex_ycoords, values, 
-        api_key, color_mapper, map_options=None, title=None):
+def plot_patches_on_gmap(vertex_xcoords, vertex_ycoords, api_key,
+        solid_fill=None, values=None, color_mapper=None, 
+        map_options=None, title=None, alpha=0.25):
+
+    if values is not None:
+        assert color_mapper is not None, "must provide a color_mapper if providing a list of colors"
+    else:
+        assert solid_fill is not None, "must provide a solid fill color if not providing a list of colors"
 
     plot = bk.gmap(api_key, map_options=map_options, title=title)
     plot.add_tools(PanTool(), WheelZoomTool(), ResetTool())
 
-    source_patches = bk.ColumnDataSource(
-        data=dict(xs=vertex_xcoords, ys=vertex_ycoords,
-                  colors=values))
+    data = dict(xs=vertex_xcoords, ys=vertex_ycoords)
+    if values is not None:
+        data['colors'] = values
 
-    patches_glyph = plot.patches('xs', 'ys', fill_alpha=0.25, 
-        fill_color={"field": "colors", "transform": color_mapper},
-        line_color='black', line_width=0, source=source_patches)
+    source_patches = bk.ColumnDataSource(data=data)
 
-    color_bar = ColorBar(color_mapper=color_mapper, 
-        border_line_color=None, location=(0,0), scale_alpha=0.25,
-        title="minutes")
+    if values is not None:
+        patches_glyph = plot.patches('xs', 'ys', fill_alpha=alpha, 
+            fill_color={"field": "colors", "transform": color_mapper},
+            source=source_patches, line_width=0)
 
-    plot.add_layout(color_bar, 'right')
+        color_bar = ColorBar(color_mapper=color_mapper, 
+            border_line_color=None, location=(0,0), scale_alpha=alpha,
+            title="minutes")
+        plot.add_layout(color_bar, 'right')
+    else:
+        patches_glyph = plot.patches('xs', 'ys', fill_alpha=alpha, 
+            fill_color=solid_fill, source=source_patches, line_width=0)
+
     return plot
 
 def main():
@@ -50,6 +62,7 @@ def main():
     parser.add_argument('pickle_dump')
     parser.add_argument('output_file')
     parser.add_argument('-c', '--config_filename', dest='config_filename', help="Config file with private info", default=None)
+    parser.add_argument('--max_happy_commute', default=45, type=float, help="For plot that overlays all the commutes, what's the longest not colored red?")
     parser.add_argument('--center_lat', default=34.053695, help="latitude to center on")
     parser.add_argument('--center_lng', default=-118.430208, help="longitutde to center on")
     parser.add_argument('--zoom', default=11, type=int, help="initial zoom of maps.  goes 1 (least zoomed) to 20 (most zoomed)")
@@ -101,14 +114,37 @@ def main():
     color_mapper = LinearColorMapper(palette=all_palettes[args.palette][args.ncolors], 
         low=args.cbar_min, high=args.cbar_max)
 
+    nhappy = np.zeros(len(xcoords), dtype=int)
     for name in names:
         for destkey in ['towork', 'tohome']:
             key = f'{name}_{destkey}'
             colors = data[key]
-            plots.append(plot_patches_on_gmap(xcoords, ycoords, colors, 
-                api_key, map_options=moptions, title=restructure_key(key),
+            plots.append(plot_patches_on_gmap(xcoords, ycoords, api_key, 
+                values=colors, map_options=moptions, title=restructure_key(key),
                 color_mapper=color_mapper))
 
+            msk = np.array(colors) <= args.max_happy_commute
+            nhappy[msk] += 1
+
+    ## now overlap all the commutes:
+    title = f'Areas where all commutes are < {args.max_happy_commute} minutes'
+    plot = plot_patches_on_gmap(
+        list(np.array(xcoords)[nhappy<len(allkeys)-1]), 
+        list(np.array(ycoords)[nhappy<len(allkeys)-1]), 
+        api_key, map_options=moptions, title=title, solid_fill='red')
+
+    data = dict(
+        xs=list(np.array(xcoords)[nhappy==len(allkeys)-1]), 
+        ys=list(np.array(ycoords)[nhappy==len(allkeys)-1]))
+
+    source_patches = bk.ColumnDataSource(data=data)
+    patches_glyph = plot.patches('xs', 'ys', fill_alpha=0.25, 
+        fill_color='orange', source=source_patches, line_width=0)
+
+    plots.append(plot)
+
+
+    ## now show
     grid = gridplot(plots, ncols=2)
     bk.show(grid)
 
